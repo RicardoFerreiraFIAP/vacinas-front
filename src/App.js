e858245fb9435404d86fcc1edc9e70ea8cde3355
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-12-23 19:29:20 -0300

    Adicionar routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+
 import "./styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
+import Cadastro from "./pages/Cadastro";
 
-function App() {
-  return <Home />;
+const Loading = () => <div className="loading">Loading...</div>;
+
+export default function App() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="cadastro" element={<Cadastro />} />
+      </Routes>
+    </Suspense>
+  );
 }
-
-export default App;
