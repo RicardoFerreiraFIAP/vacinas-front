fcb1fed9985e8e5477afc517966e5aadbca50a60
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-12-24 16:59:20 -0300

    Inclui arquivo que realiza conexão com API

---
+++ src/services/api.js
@@ -0,0 +1,58 @@
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import getFormData from "../utils/getFormData";
+
+export async function getPatientInfo(event) {
+  console.log("inside");
+  const URL = "URL";
+  const RESOURCE = "resource";
+  const RESOURCEIDENTIFIER = "identifier";
+  const completeUrl = `${URL}/${RESOURCE}/${RESOURCEIDENTIFIER}`;
+  const MySwal = withReactContent(Swal);
+
+  try {
+    const response = await fetch(completeUrl);
+    const convertedResponse = await response.json();
+    //todo: formatData()
+    //todo: disparar swal com dados
+    //todo: se nao tiver dados avisar que nao esta cadastrado
+    return convertedResponse;
+  } catch (error) {
+    console.log("errou", error);
+    return await MySwal.fire({
+      title: <p>Oops! Aconteceu um erro!</p>,
+      text: "Não foi possível recuperar seus dados. Por favor, tente novamente.",
+      icon: "error",
+    });
+  }
+}
+
+export async function setPatientInfo(event) {
+  console.log("inside");
+  event.preventDefault();
+  const URL = "URL";
+  const RESOURCE = "resource";
+  const RESOURCEIDENTIFIER = "identifier";
+  const completeUrl = `${URL}/${RESOURCE}/${RESOURCEIDENTIFIER}`;
+  const MySwal = withReactContent(Swal);
+
+  try {
+    const formData = getFormData();
+    const settings = {
+      method: "POST",
+      body: JSON.stringify({ ...formData }),
+    };
+    const response = await fetch(completeUrl, settings);
+    if (response.ok) return true;
+    //Todo: pegar erro?
+    const convertedResponse = await response.json();
+    return convertedResponse;
+  } catch (error) {
+    console.log("errou", error);
+    await MySwal.fire({
+      title: <p>Oops! Aconteceu um erro!</p>,
+      text: "Não foi possível recuperar seus dados. Por favor, tente novamente.",
+      icon: "error",
+    });
+  }
+}
