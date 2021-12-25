0bd66e236753a23de2d43b0e9bf1e804a39d6fbc
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-12-25 14:36:08 -0300

    Inclui modal com informações para o professor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 import "./styles/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
 import Cadastro from "./pages/Cadastro";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const Loading = () => <div className="loading">Loading...</div>;
 
 export default function App() {
+  const [hasInfoModalAppeared, setInfoModalAppeared] = useState(false);
+
+  useEffect(handleInfoModalToTeacher);
+
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
@@ -18,4 +24,30 @@ export default function App() {
       </Routes>
     </Suspense>
   );
+
+  function handleInfoModalToTeacher() {
+    const MySwal = withReactContent(Swal);
+    if (!hasInfoModalAppeared) {
+      setTimeout(async () => {
+        await MySwal.fire({
+          title: <p>Informação sobre o trabalho</p>,
+          html: (
+            <p>
+              Professor, para testar o nosso trabalho, por favor ler o{" "}
+              <a
+                href="https://github.com/Vacinas-Backend/vacinas-front"
+                target="_blank"
+                rel="noreferrer"
+              >
+                README
+              </a>
+              &nbsp; com as instruções necessárias.
+            </p>
+          ),
+          icon: "info",
+        });
+        setInfoModalAppeared(true);
+      }, 1000);
+    }
+  }
 }
