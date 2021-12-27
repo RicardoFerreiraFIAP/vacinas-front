43f7630ca608be0d690c4b887d8a05f34ce63e17
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-12-27 10:59:45 -0300

    Passa estado para filhos e cria hook de useEffect para atualizar dados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,26 @@ import VacinaStats from "../components/VacinaStats";
 import Menu from "../components/Menu";
 import CronogramaDeVacinacao from "../components/CronogramaDeVacinacao";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
+import { getPatientInfo } from "../services/api";
 
-export default function Home() {
+export default function Home(props) {
+  const { patientInfo, setPatientInfo } = props;
+
+  useEffect(() => {
+    async function fetchData() {
+      const updatedPatientInfo = await getPatientInfo();
+      console.log({ updatedPatientInfo });
+      setPatientInfo(updatedPatientInfo);
+    }
+    fetchData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>
       <Header />
       <VacinaStats />
-      <Menu />
+      <Menu patientInfo={patientInfo} />
       <CronogramaDeVacinacao />
       <Footer />
     </div>
